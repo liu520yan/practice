017ab54f26c089998fb8e7fa99b3d7236d7bdc57
liuyan <yan.liu@@timanetworks.com>
2017-10-31 12:04:52 +0800

    rosources 注册进eureka

--- resources/src/main/java/com/liuyan/resources/ResourcesApplication.java
+++ resources/src/main/java/com/liuyan/resources/ResourcesApplication.java
@@ -2,15 +2,18 @@ package com.liuyan.resources;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.annotation.Resource;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by liuyan on 2017/10/10.
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ResourcesApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ResourcesApplication.class, args);
+        new SpringApplicationBuilder(
+                ResourcesApplication.class)
+                .web(true).run(args);
     }
 }
