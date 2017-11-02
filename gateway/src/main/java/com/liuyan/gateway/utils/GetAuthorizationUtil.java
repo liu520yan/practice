ae15500460e7f3c655e6b7667d06ad484af3e02b
liu520yan <1027552153@qq.com>
2017-11-02 23:17:40 +0800

    添加dockerfile，添加pom文件

--- gateway/src/main/java/com/liuyan/gateway/utils/GetAuthorizationUtil.java
+++ gateway/src/main/java/com/liuyan/gateway/utils/GetAuthorizationUtil.java
@@ -16,10 +16,10 @@ public class GetAuthorizationUtil {
         return null;
     }
 
-    public static void main(String[] args) {
-        Jwt jwt = JwtHelper.decode("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.mMMWrQZZAiVWAp5ekR7h8sRpB3NjPM8e2UPof0qiohZi6Mdf4V1xWs79Pu63bmcdXdZV2binlIaW0Qs410pjIjVKrmnO0NHwPch7AvMYgBZ64b2dW-ViTKNyHAGW_faV-W7bfDzsV8b7oxnq9YKUt8EpfXWIQ-x58U-2pQT2NzHjO6e9igu2vBTt2kUpYhusO0vAwYFLkVqkit7WurpoExlUPUmk9icQzde2nfTEGMz1hlB5HVnYGgKkdKDQaDO4tUvJrz3Mkl2WpVTqGb7Aa_s3RvWHCKYKTiHUuGQSkWHVgggddvTUXAiKtwI-EcaUqiNVtGOlwc5Y8pCnIA3RIQ");
-        System.out.println(jwt);
-
-    }
+//    public static void main(String[] args) {
+//        Jwt jwt = JwtHelper.decode("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.mMMWrQZZAiVWAp5ekR7h8sRpB3NjPM8e2UPof0qiohZi6Mdf4V1xWs79Pu63bmcdXdZV2binlIaW0Qs410pjIjVKrmnO0NHwPch7AvMYgBZ64b2dW-ViTKNyHAGW_faV-W7bfDzsV8b7oxnq9YKUt8EpfXWIQ-x58U-2pQT2NzHjO6e9igu2vBTt2kUpYhusO0vAwYFLkVqkit7WurpoExlUPUmk9icQzde2nfTEGMz1hlB5HVnYGgKkdKDQaDO4tUvJrz3Mkl2WpVTqGb7Aa_s3RvWHCKYKTiHUuGQSkWHVgggddvTUXAiKtwI-EcaUqiNVtGOlwc5Y8pCnIA3RIQ");
+//        System.out.println(jwt);
+//
+//    }
 
 }
