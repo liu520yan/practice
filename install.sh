74de063d1076770158ba7be73e364113a9d1c661
liuyan <yan.liu@@timanetworks.com>
2018-02-13 10:45:37 +0800

    update docker config

--- install.sh
+++ install.sh
@@ -3,9 +3,21 @@
 PARENT_DIR=`dirname $0 | pwd`
 
 EUREKA_PATH=$PARENT_DIR/eureka
+CONFIG_PATH=$PARENT_DIR/config
+GATEWAY_PATH=$PARENT_DIR/gateway
+OAUTH2_PATH=$PARENT_DIR/oauth2
+RESOURCES_PATH=$PARENT_DIR/resources
 
 VERSION="1.0.1-SNAPSHOT"
 EUREKA_IMAGE="registry.xuey.top/eureka:$VERSION"
+
+CONFIG_IMAGE="registry.xuey.top/config:$VERSION"
+
+GATEWAY_IMAGE="registry.xuey.top/gateway:$VERSION"
+
+OAUTH2_IMAGE="registry.xuey.top/oauth2:$VERSION"
+
+RESOURCES_IMAGE="registry.xuey.top/resources:$VERSION"
 
 function installImage(){
 
@@ -30,6 +42,26 @@ function installEureka(){
     installImage $EUREKA_IMAGE
 }
 
+function installConfig(){
+    cd $CONFIG_PATH
+    installImage $CONFIG_IMAGE
+}
+
+function installGateway(){
+    cd $GATEWAY_PATH
+    installImage $GATEWAY_IMAGE
+}
+
+function installOauth2(){
+    cd $OAUTH2_PATH
+    installImage $OAUTH2_IMAGE
+}
+
+function installResources(){
+    cd $RESOURCES_PATH
+    installImage $RESOURCES_IMAGE
+}
+
 function package(){
     mvn -DskipTests clean package
 }
@@ -38,13 +70,33 @@ case $1 in
     package)
     package
     ;;
-    server)
+    eureka)
     package
     installEureka
+    ;;
+    config)
+    package
+    installConfig
+    ;;
+    oauth2)
+    package
+    installOauth2
+    ;;
+    resources)
+    package
+    installResources
+    ;;
+    gateway)
+    package
+    installGateway
     ;;
     *)
     package
     installEureka
+    installConfig
+    installOauth2
+    installResources
+    installGateway
 esac
 
 
